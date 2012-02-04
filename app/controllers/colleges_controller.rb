0e0770291bd9246282e5d584f5aa5b61c3d5f675
Wouter Timmermans <protowouter@gmail.com>
2012-02-05 00:07:03 +0100

    complete college model functionality

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -1,5 +1,6 @@
 class CollegesController < ApplicationController
   def index
+    @colleges = College.all
   end
 
   def new
@@ -7,8 +8,18 @@ class CollegesController < ApplicationController
   end
 
   def create
+    college = College.new(params[:college])
+    if college.save
+      redirect_to colleges_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+     college = College.find(params[:id])
+      college.destroy
+      flash[:success] = "Universiteit/Hogeschool verwijderd"
+        redirect_to colleges_path
   end
 end
