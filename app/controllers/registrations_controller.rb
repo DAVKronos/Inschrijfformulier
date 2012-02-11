1e2f0e273b9a7d99409fa396bbc302308bc376e4
Wouter Timmermans <protowouter@gmail.com>
2012-02-11 02:52:02 +0100

    Form working kind of

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,22 +4,46 @@ class RegistrationsController < ApplicationController
   end
 
   def new
-    @registration = Registration.new
-    @events = Event.all
+    session[:registration_params] ||= {}
+    session[:participations] ||= []
+    @registration = Registration.new(session[:registration_params])
+    @registration.current_step = session[:registration_step]
+    @event_participations = @registration.events
     @event_participation = EventParticipation.new
+    @events = Event.all
   end
 
   def create
-    registration = Registration.new(params[:registration])
-    registration.event_participations.build(params[:event_participation])
-   
-   if registration.save
-      flash[:success] = "Inschrijving voltooid"
-      redirect_to registration
-    else
-      render 'new'
+    session[:registration_params].deep_merge!(params[:registration]) if params[:registration]
+      @registration = Registration.new(session[:registration_params])
+      @registration.current_step = session[:registration_step]
+      session[:participations].each do |parti|
+        @registration.event_participations.build(parti)
+        puts session[:participations]
+      end
+      @event_participation = EventParticipation.new
+      @events = Event.where("sex_id = '#{@registration.sex.id}'")
+      @event_participations = @registration.event_participations
+      if @registration.valid?
+        if params[:back_button]
+          @registration.previous_step
+        elsif params[:new_participation_button]
+          session[:participations] << params[:event_participation]
+        elsif @registration.last_step?
+          @registration.save if @registration.all_valid?
+        else
+          @registration.next_step
+        end
+        session[:registration_step] = @registration.current_step
+      end
+      if @registration.new_record?
+        render "new"
+      else
+        session[:registration_step] = session[:registration_params] = nil
+        flash[:notice] = "Inschrijving bevestigd!"
+        redirect_to @registration
+      end
     end
-  end
   
   def edit
     @registration = Registration.find(params[:id])
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -4,6 +4,39 @@ class Registration < ActiveRecord::Base
   belongs_to :college
   has_many :event_participations, :dependent => :destroy
   has_many :events, :through => :event_participations
+  
+  attr_writer :current_step
+  
+  def current_step
+    @current_step || steps.first
+  end
+
+  def steps
+    %w[general participation athlete volunteer secondaries payment confirmation]
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step)-1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+  def all_valid?
+    steps.all? do |step|
+      self.current_step = step
+      valid?
+    end
+  end
 end
 # == Schema Information
 #
