2dfbc3529653f7d6c74d37333b42879e174951de
Wouter Timmermans <protowouter@gmail.com>
2012-02-14 17:56:09 +0100

    (Somewhat) final css

---
+++ app/assets/javascripts/registration.js
@@ -0,0 +1,22 @@
+jQuery.fn.center = function () {
+  this.css("position","absolute");
+  this.css("top", ( $(window).height() - this.height() ) / 2+$(window).scrollTop() + "px");
+  this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");
+  return this;
+}
+
+jQuery.fn.centerWidth = function () {
+  this.css("position","absolute");
+  this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");
+  this.css("top", ( this.height() /2 -20));
+  return this;
+}
+
+$(document).ready( function(){
+//  $("#registration_form").center();
+//  $("#registration_image").centerWidth();
+  $(window).resize(function(){
+//    $("#registration_form").center();
+//    $("#registration_image").centerWidth();
+  });
+  });
