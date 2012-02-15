c2648b915403758f1756ae0dedb0fc509a0c36f1
Wouter Timmermans <protowouter@gmail.com>
2012-02-15 12:12:31 +0100

    Looks promising

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-tooltip
+//= require bootstrap
 //= require_tree .
 //= require rails.validations
 $(document).ready(function() {
